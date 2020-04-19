20059461b11bba5a77e57acf82e54604003ce75d
welvim <wahyuajisulaiman@gmail.com>
2020-04-20 01:56:49 +0700

    update sql seed

--- ta_sbd.sql
+++ ta_sbd.sql
@@ -1,186 +1,145 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 5.7.28, for Linux (x86_64)
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Apr 19, 2020 at 04:36 PM
--- Server version: 8.0.19
--- PHP Version: 7.2.24-0ubuntu0.18.04.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: ta_sbd
+-- ------------------------------------------------------
+-- Server version	5.7.28-0ubuntu0.19.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `ta_sbd`
---
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `bookmarks`
 --
 
+DROP TABLE IF EXISTS `bookmarks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `bookmarks` (
-  `id` int NOT NULL,
-  `id_user` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
   `name_github` varchar(255) NOT NULL,
   `avatar` varchar(255) NOT NULL,
   `url` varchar(255) NOT NULL,
-  `id_directory` int NOT NULL,
+  `id_directory` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` int NOT NULL,
-  `star` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `status` int(11) NOT NULL,
+  `star` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_user` (`id_user`),
+  KEY `id_directory` (`id_directory`),
+  CONSTRAINT `bookmarks_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `bookmarks_ibfk_2` FOREIGN KEY (`id_directory`) REFERENCES `directories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `bookmarks`
+--
+
+LOCK TABLES `bookmarks` WRITE;
+/*!40000 ALTER TABLE `bookmarks` DISABLE KEYS */;
+INSERT INTO `bookmarks` VALUES (3,1,'Wahyu Aji Sulaiman','https://avatars0.githubusercontent.com/u/38114768?v=4','https://github.com/claytten',1,'2020-04-19 18:51:03',1,3),(6,7,'Luqman','https://avatars0.githubusercontent.com/u/38354405?v=4','https://github.com/luqmansen',1,'2020-04-19 18:53:47',1,12),(7,7,'Yoffa','https://avatars2.githubusercontent.com/u/34566219?v=4','https://github.com/sayyidyofa',1,'2020-04-19 18:53:52',1,11);
+/*!40000 ALTER TABLE `bookmarks` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `directories`
 --
 
+DROP TABLE IF EXISTS `directories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `directories` (
-  `id` int NOT NULL,
-  `folder` varchar(60) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `folder` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `directories`
 --
 
-INSERT INTO `directories` (`id`, `folder`) VALUES
-(1, '/');
-
--- --------------------------------------------------------
+LOCK TABLES `directories` WRITE;
+/*!40000 ALTER TABLE `directories` DISABLE KEYS */;
+INSERT INTO `directories` VALUES (1,'/');
+/*!40000 ALTER TABLE `directories` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `history`
 --
 
+DROP TABLE IF EXISTS `history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `history` (
-  `id` int NOT NULL,
-  `id_user` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
   `name_github` varchar(255) NOT NULL,
   `avatar` varchar(255) NOT NULL,
   `url` varchar(255) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `status` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_user` (`id_user`),
+  CONSTRAINT `history_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `history`
 --
 
-INSERT INTO `history` (`id`, `id_user`, `name_github`, `avatar`, `url`, `date`, `status`) VALUES
-(1, 1, 'Wahyu Aji Sulaiman', 'https://avatars0.githubusercontent.com/u/38114768?v=4', 'https://github.com/claytten', '2020-04-19 14:24:15', 0),
-(2, 1, 'Fanny Hasbi', 'https://avatars1.githubusercontent.com/u/11705665?v=4', 'https://github.com/fannyhasbi', '2020-04-19 14:27:36', 0);
-
--- --------------------------------------------------------
+LOCK TABLES `history` WRITE;
+/*!40000 ALTER TABLE `history` DISABLE KEYS */;
+INSERT INTO `history` VALUES (3,1,'Wahyu Aji Sulaiman','https://avatars0.githubusercontent.com/u/38114768?v=4','https://github.com/claytten','2020-04-19 18:48:35',1),(4,1,'Fanny Hasbi','https://avatars1.githubusercontent.com/u/11705665?v=4','https://github.com/fannyhasbi','2020-04-19 18:48:40',0),(9,6,'anonymousliem','https://avatars3.githubusercontent.com/u/38047246?v=4','https://github.com/anonymousliem','2020-04-19 18:50:32',0),(10,6,'','https://avatars2.githubusercontent.com/u/38581836?v=4','https://github.com/shidqiiii','2020-04-19 18:50:46',0),(11,7,'Yoffa','https://avatars2.githubusercontent.com/u/34566219?v=4','https://github.com/sayyidyofa','2020-04-19 18:53:38',1),(12,7,'Luqman Setyo Nugroho','https://avatars0.githubusercontent.com/u/38354405?v=4','https://github.com/luqmansen','2020-04-19 18:53:43',1);
+/*!40000 ALTER TABLE `history` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `roles` varchar(255) NOT NULL,
-  `status` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `status` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `username`, `password`, `roles`, `status`) VALUES
-(1, 'Users', 'users', '9bc65c2abec141778ffaa729489f3e87', 'user', 1),
-(2, 'Admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', 1);
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Usersd','users','9bc65c2abec141778ffaa729489f3e87','user',1),(2,'Admin','admin','21232f297a57a5a743894a0e4a801fc3','admin',1),(4,'admin2','admin2','c84258e9c39059a89ab77d846ddab909','admin',1),(6,'users2','users2','86425c35a33507d479f71ade53a669aa','user',1),(7,'users1','users1','b2255c5d029d69e44374a99e9d03921d','user',1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `bookmarks`
---
-ALTER TABLE `bookmarks`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_user` (`id_user`),
-  ADD KEY `id_directory` (`id_directory`);
-
---
--- Indexes for table `directories`
---
-ALTER TABLE `directories`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `history`
---
-ALTER TABLE `history`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_user` (`id_user`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `bookmarks`
---
-ALTER TABLE `bookmarks`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `directories`
---
-ALTER TABLE `directories`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `history`
---
-ALTER TABLE `history`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `bookmarks`
---
-ALTER TABLE `bookmarks`
-  ADD CONSTRAINT `bookmarks_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `bookmarks_ibfk_2` FOREIGN KEY (`id_directory`) REFERENCES `directories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `history`
---
-ALTER TABLE `history`
-  ADD CONSTRAINT `history_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-04-20  1:55:22
